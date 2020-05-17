e9c42cae58cd7d037b0192743e1be8c648ab9366
zhangzhendong <zhangzhendong@bogon.corp.ncfgroup.com>
2020-05-17 19:36:28 +0800

    添加paystore

---
+++ RCloudMessage/Sections/Me/PaymentManager.h
@@ -0,0 +1,17 @@
+//
+//  PaymentManager.h
+//  SealTalk
+//
+//  Created by zhangzhendong on 2020/5/17.
+//  Copyright © 2020 RongCloud. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PaymentManager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
